64cb51532a1536650d5251cdc1de0440f93b94d3
rkdev1111 <106636875+rkdev1111@users.noreply.github.com>
2024-07-10 22:23:36 -0300

    hosted zones/cert mngr + alb config

--- load-balancer-target-group/main.tf
+++ load-balancer-target-group/main.tf
@@ -14,13 +14,13 @@ resource "aws_lb_target_group" "dev_proj_1_lb_target_group" {
   protocol = var.lb_target_group_protocol
   vpc_id   = var.vpc_id
   health_check {
-    path = "/login"
-    port = 8080
-    healthy_threshold = 6
+    path                = "/login"
+    port                = 8080
+    healthy_threshold   = 6
     unhealthy_threshold = 2
-    timeout = 2
-    interval = 5
-    matcher = "200"  # has to be HTTP 200 or fails
+    timeout             = 2
+    interval            = 5
+    matcher             = "200" # has to be HTTP 200 or fails
   }
 }
 
--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ module "jenkins" {
   enable_public_ip_address  = true
   user_data_install_jenkins = templatefile("./jenkins-runner-script/jenkins-installer.sh", {})
 }
-/*
+
 module "lb_target_group" {
   source                   = "./load-balancer-target-group"
   lb_target_group_name     = "jenkins-lb-target-group"
@@ -36,34 +36,34 @@ module "lb_target_group" {
 }
 
 module "alb" {
-  source                    = "./load-balancer"
-  lb_name                   = "dev-proj-1-alb"
-  is_external               = false
-  lb_type                   = "application"
-  sg_enable_ssh_https       = module.security_group.sg_ec2_sg_ssh_http_id
-  subnet_ids                = tolist(module.networking.dev_proj_1_public_subnets)
-  tag_name                  = "dev-proj-1-alb"
-  lb_target_group_arn       = module.lb_target_group.dev_proj_1_lb_target_group_arn
-  ec2_instance_id           = module.jenkins.jenkins_ec2_instance_ip
-  lb_listner_port           = 80
-  lb_listner_protocol       = "HTTP"
-  lb_listner_default_action = "forward"
-  lb_https_listner_port     = 443
-  lb_https_listner_protocol = "HTTPS"
-  dev_proj_1_acm_arn        = module.aws_ceritification_manager.dev_proj_1_acm_arn
+  source                          = "./load-balancer"
+  lb_name                         = "dev-proj-1-alb"
+  is_external                     = false
+  lb_type                         = "application"
+  sg_enable_ssh_https             = module.security_group.sg_ec2_sg_ssh_http_id
+  subnet_ids                      = tolist(module.networking.dev_proj_1_public_subnets)
+  tag_name                        = "dev-proj-1-alb"
+  lb_target_group_arn             = module.lb_target_group.dev_proj_1_lb_target_group_arn
+  ec2_instance_id                 = module.jenkins.jenkins_ec2_instance_ip
+  lb_listner_port                 = 80
+  lb_listner_protocol             = "HTTP"
+  lb_listner_default_action       = "forward"
+  lb_https_listner_port           = 443
+  lb_https_listner_protocol       = "HTTPS"
+  dev_proj_1_acm_arn              = module.aws_ceritification_manager.dev_proj_1_acm_arn
   lb_target_group_attachment_port = 8080
 }
 
 module "hosted_zone" {
   source          = "./hosted-zone"
-  domain_name     = "jenkins.jhooq.org"
+  domain_name     = "jenkins.klimb.site"
   aws_lb_dns_name = module.alb.aws_lb_dns_name
   aws_lb_zone_id  = module.alb.aws_lb_zone_id
 }
 
 module "aws_ceritification_manager" {
   source         = "./certificate-manager"
-  domain_name    = "jenkins.jhooq.org"
+  domain_name    = "jenkins.klimb.site"
   hosted_zone_id = module.hosted_zone.hosted_zone_id
 }
-*/
+
